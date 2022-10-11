d60a9bb293821a0989c97341fa72a2c9ced7a9fe
vanessajchoi <vanessajchoi@gmail.com>
2022-10-11 16:16:51 +0900

    html > React

---
+++ pro_htmltoreact/src/App.js
@@ -0,0 +1,16 @@
+import './sytles/index.css'
+import './App.css';
+import MyNav from './components/MyNav';
+import MyHeader from './components/MyHeader';
+import MyMain from './components/MyMain';
+
+function App() {
+  return (
+      <>
+        <MyNav />
+        <MyHeader />
+        <MyMain />
+      </>
+  );
+}
+export default App;
