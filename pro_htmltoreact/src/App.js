b3ce9837d4571a04bad515621058a8c4d6160d1c
vanessajchoi <vanessajchoi@gmail.com>
2022-10-11 17:18:11 +0900

    html > React

--- pro_htmltoreact/src/App.js
+++ pro_htmltoreact/src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import MyNav from './components/MyNav';
 import MyHeader from './components/MyHeader';
 import MyMain from './components/MyMain';
-
+import MyFooter from './components/MyFooter';
 function App() {
   return (
       <>
         <MyNav />
         <MyHeader />
         <MyMain />
+        <MyFooter />
       </>
   );
 }
