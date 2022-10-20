98251b0e1ad3a18b9c148eddd1bb207881c5c5db
vanessajchoi <vanessajchoi@gmail.com>
2022-10-20 18:39:09 +0900

    날씨 정보 출력

--- weather/src/components/WeatherDay.js
+++ weather/src/components/WeatherDay.js
@@ -10,16 +10,32 @@ export default function WeatherDay(probs){
     for(let k of keys) {
         items.push(witem[k]);
     }
-   
+    console.log(items);
     
     return(
         <>
         {/* <h2> {probs.d} </h2> */}
         <ul>
-            <li> {d}일 후 오전 강수량 : {}</li>
-            <li> {d}일 후 오전 강수량 : {}</li>
-            <li> {d}일 후 오전 날씨예보 : {}</li>
-            <li> {d}일 후 오후 날씨예보 : {}</li>
+            <li className='rntotalam'> 
+                <span className='dayafter'> {d}일 후 </span> 
+                <span className='rneky'> 오전 강수량 </span> 
+                <span className='rnvalue'> {items[0]} </span> 
+            </li>
+            <li className='rntotalpm'> 
+                <span className='dayafter'> {d}일 후 </span> 
+                <span className='rneky'> 오후 강수량 </span> 
+                <span className='rnvalue'> {items[1]} </span> 
+            </li>
+            <li className='wtotalam'> 
+                <span className='dayafter'> {d}일 후 </span> 
+                <span className='wkey'> 오전 날씨예보</span> 
+                <span className='rnvalue'> {items[2]} </span>
+            </li>
+            <li className='wtotalpm'> 
+                <span className='dayafter'> {d}일 후 </span> 
+                <span className='wkey'> 오후 날씨예보 </span>
+                <span className='rnvalue'> {items[3]} </span>
+            </li>
         </ul>
         </>
     );
